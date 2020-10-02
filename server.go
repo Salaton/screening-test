d1b0f52e91e01619a1207826e3b5e37f7abd6916
Sala <elvisdenis056@gmail.com>
2020-10-02 20:51:03 +0300

    fixed connection to the database

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/Salaton/screening-test.git/graph"
 	"github.com/Salaton/screening-test.git/graph/generated"
+	db "github.com/Salaton/screening-test.git/postgres"
 )
 
 const defaultPort = "8080"
@@ -24,6 +25,17 @@ func main() {
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
+	if err := InitDB(); err != nil {
+		log.Fatalf("FAILED TO CONNECT TO DB %v", err.Error())
+	}
+
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// InitDB function to start the db connections process
+func InitDB() error {
+	graph.DB = &db.PostgresClient{}
+	dsn := "user=sala password=$krychowiak-254$ dbname=savannahtest port=5432 sslmode=disable TimeZone=Africa/Nairobi"
+	return graph.DB.Open(dsn)
 }
