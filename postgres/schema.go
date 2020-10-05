4a8520a648bb4506ba69cd7849c234e87d139f19
Sala <elvisdenis056@gmail.com>
2020-10-05 13:46:35 +0300

    changed the schema

--- postgres/schema.go
+++ postgres/schema.go
@@ -1,22 +1,50 @@
 package postgres
 
+// // Here we declare our models --> normal structs with basic Go types..
 import (
-	model "github.com/Salaton/screening-test.git/graph/model"
+	"time"
 )
 
-// Using a custom schema for the one-to-many relationship..
 type Customer struct {
 	ID          int      `json:"id" gorm:"primaryKey"`
 	Name        string   `json:"name"`
 	Phonenumber int      `json:"Phonenumber"`
 	Email       string   `json:"Email"`
-	Orders      []*Order `json:"orders" gorm:"foreignKey:CustomerID;"`
+	Orders      []*Order `json:"orders"`
+}
+
+type Item struct {
+	ID       int    `json:"id" gorm:"primaryKey"`
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+	OrderID  uint   `json:"-"`
+}
+
+type ItemInput struct {
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
 }
 
 type Order struct {
-	ID              int64              `json:"id" gorm:"primaryKey"`
-	Item            []*model.ItemInput `json:"item" gorm:"-"`
-	Price           float64            `json:"price"`
-	DateOrderPlaced string             `json:"date_order_placed"`
-	CustomerID      int
+	ID                  int       `json:"id" gorm:"primaryKey"`
+	CustomerName        string    `json:"customerName"`
+	CustomerPhoneNumber int       `json:"customerPhoneNumber"`
+	Item                []*Item   `json:"item"`
+	Price               float64   `json:"price"`
+	DateOrderPlaced     time.Time `json:"date_order_placed"`
+	CustomerID          int       `json:"-"`
+}
+
+type CustomerInput struct {
+	Name        string `json:"name"`
+	Phonenumber int    `json:"Phonenumber"`
+	Email       string `json:"Email"`
+}
+
+type OrderInput struct {
+	Customername        string       `json:"customername"`
+	CustomerPhoneNumber int          `json:"customerPhoneNumber"`
+	Item                []*ItemInput `json:"item"`
+	Price               float64      `json:"price"`
+	DateOrderPlaced     time.Time    `json:"date_order_placed"`
 }
