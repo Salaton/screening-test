3d9857b0060204e1d6b69df328a33e3980052ac3
Sala <elvisdenis056@gmail.com>
2020-10-06 17:38:41 +0300

    consumed africas talking API to send notificatons

--- notification/notification.go
+++ notification/notification.go
@@ -3,21 +3,20 @@ package notification
 import (
 	"fmt"
 
-	db "github.com/Salaton/screening-test.git/postgres"
 	"github.com/davidmutia47/AfricasTalkingGateway"
 )
 
-func sendNotification() {
-	var DB db.DBClient
-	DB.FetchPhoneNumber()
+func SendNotification(customername, phonenumber string) string {
+	// var DB db.DBClient
+	// DB.FetchPhoneNumber()
 	// Specify your login credentials
 	username := "drowsydriver"
 	apikey := "a7f07c892fb5a74e5b90481a10a72154893f216fd85c71e2271b21167fa56556"
 	// Specify the numbers that you want to send to in a comma-separated list
 	// Please ensure you include the country code (+254 for Kenya in this case)
-	recipients := "254719158559"
+	recipients := phonenumber
 	// And of course we want our recipients to know what we really do
-	message := "Hello, world"
+	message := "Hello there " + customername + " Your order has been received"
 
 	//Create instance of getWay
 	getWay := AfricasTalkingGateway.AfricasTalkingGateway(username, apikey)
@@ -33,4 +32,5 @@ func sendNotification() {
 		//handle error
 	}
 	fmt.Println(response)
+	return message
 }
