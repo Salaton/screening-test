beb158cc3bfc9407203cda6cf0d95e9d79b8c7a8
Sala <elvisdenis056@gmail.com>
2020-10-14 00:19:12 +0300

    added a db reference to the middleware func parameter

--- auth/middleware.go
+++ auth/middleware.go
@@ -16,36 +16,29 @@ type contextKey struct {
 	name string
 }
 
-func Middleware() func(http.Handler) http.Handler {
+func Middleware(db db.DBClient) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			header := r.Header.Get("Authorization")
-			// splitToken := strings.Split(reqToken, "Bearer ")
-			// reqToken = splitToken[1]
 
-			// Allow unauthenticated users in
 			if header == "" {
 				next.ServeHTTP(w, r)
 				return
 			}
 
 			//validate jwt token
-			// tokenStr := header
-			// Should return the Username that was used to generate the token
+
 			username, err := ParseToken(header)
 			if err != nil {
 				http.Error(w, "Invalid token", http.StatusForbidden)
 				return
 			}
 
-			//check if user exists in db
-			user, err := DB.GetUser(username)
-			// var user model.User
-			// row := ps.db.Table("users").Where("username = ?", username).Select("id,username").Row()
-			// row.Scan(&user)
+			// //check if user exists in db
+			user, err := db.GetUser(username)
+
 			if err != nil {
 				next.ServeHTTP(w, r)
-
 				return
 			}
 
