c78e9ad210efb8604c7845cdc0368efaa22775b1
Sala <elvisdenis056@gmail.com>
2020-10-18 22:35:26 +0300

    tweaked functions

--- tests/functions_test.go
+++ tests/functions_test.go
@@ -33,10 +33,10 @@ func TestHashPassword(t *testing.T) {
 }
 
 func TestToken(t *testing.T) {
-	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNhbGF0In0.gvvpBiHU_KRXEw5i5VNpj-bBGweQzQmUDjQ_5bt-KEs"
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbGFAZ21haWwuY29tIn0.L7YVb0-cTpAPsBR28voYorSCx7I_Bp663bpcsgbX99M"
 
 	t.Run("Creating a new token", func(t *testing.T) {
-		got, _ := auth.CreateNewToken("Salat")
+		got, _ := auth.CreateNewToken("sala@gmail.com")
 		want := tokenString
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -45,7 +45,7 @@ func TestToken(t *testing.T) {
 
 	t.Run("Parse the token", func(t *testing.T) {
 		got, _ := auth.ParseToken(tokenString)
-		want := "Salat"
+		want := "sala@gmail.com"
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
